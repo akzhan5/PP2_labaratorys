979980e1093750f10f80fc9fb720e76981500cf6
Akzhan <akzhansabibolda5@gmail.com>
2023-01-21 17:24:13 +0600

    Added the first lab with examples from w3school

---
+++ lab1/syntax.py
@@ -0,0 +1,26 @@
+#indentation is inmortant - right one:
+if 5>2: 
+    print('Five is greater than two!')
+
+#wrong one - synax error: 
+if 5>2: 
+print('Five is greater than two')
+
+#number of spaces is up to me: 
+if 5>2: 
+    print('Five is greater than two!')
+if 5>2: 
+        print('Five is greater than two!')
+
+#error: 
+if 5>2:
+    print('Five is grater than two!')
+        print('Five is greater than two!')
+
+#variables
+x = 5
+y = "Hello, World!"
+
+#comments: 
+#This is a comment.
+print("Hello, World!")
